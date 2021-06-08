fc5d240cca7d034028c4fc3d5c66e30b58ef9b34
oliver Gervais <46907550+OliverGervais@users.noreply.github.com>
2021-06-07 23:30:52 -0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,20 +8,12 @@ function Contact() {
         <animated.div style={props}>
         <div className=" bg-black rounded-lg xl:mx-56 mt-2 grid grid-flow-col grid-cols-1  gap-2 bg-opacity-90"> 
                                     <ul className=" flex flex-col text-white">
-                                        <li className=" mb-8 text-3xl sm:text-5xl mx-auto font-sans italic text-aqua underline "><p>Contact Me Here</p></li>
-                                        <li className="text-xl sm:text-3xl mx-auto font-sans"> <p>Phone: (631) 309-1607</p></li>
-                                        <li className="text-xl sm:text-3xl mx-auto font-sans"><p>Email: oliverg.codes@gmail.com</p></li>
+                                         <a href="https://www.facebook.com/olly.mah.71" target="_blank" rel="noopener noreferrer" ><li className=" mb-8 text-3xl sm:text-5xl mx-auto font-sans italic text-aqua underline "><p>Contact Me Here</p></li></a>
+                                      
                                         
                                     </ul>
                                 </div>
-                                <div className=" bg-white rounded-lg  xl:mx-56  mt-2 flex items-end py-4 px-2 justify-center "> 
-                               
-                                        <a href="https://www.facebook.com/olly.mah.71" target="_blank" rel="noopener noreferrer" ><img className="w-10 sm:w-20 lg:mx-6 mx-2  " src="f_logo_RGB-Blue_114.png" alt="f_logo"/></a>
-                                        <a href="https://twitter.com/OliverGervais" target="_blank" rel="noopener noreferrer" ><img className="w-10 sm:w-20  mx-2 lg:mx-6 " src="Twitter social icons - rounded square - blue.png" alt="f_logo"/></a>
-                                        <a href="https://www.linkedin.com/in/oliver-gervais-129674208/" target="_blank" rel="noopener noreferrer" ><img className="w-10 sm:w-20 mx-2 lg:mx-6 " src="LI-In-Bug.png" alt="f_logo"/></a>
-                                        <a href="https://github.com/OliverGervais" target="_blank" rel="noopener noreferrer" ><img className="w-10 sm:w-20 mx-2 lg:mx-6 " src="GitHub-Mark-120px-plus.png" alt="f_logo"/></a>
-                                        
-                                </div>
+                                
                                 </animated.div>
             
         </>
