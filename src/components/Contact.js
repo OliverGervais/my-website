1264b222596bdd8628190525dbc1fc912bc88d90
oliver Gervais <46907550+OliverGervais@users.noreply.github.com>
2021-06-07 23:33:49 -0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,9 @@ function Contact() {
     return (
         <>
         <animated.div style={props}>
-        <div className=" bg-black rounded-lg xl:mx-56 mt-2 grid grid-flow-col grid-cols-1  gap-2 bg-opacity-90"> 
+        <div className=" bg-black rounded-lg xl:mx-56 mt-2  bg-opacity-90"> 
                                     <ul className=" flex flex-col text-white">
-                                         <a href="https://www.facebook.com/olly.mah.71" target="_blank" rel="noopener noreferrer" ><li className=" mb-8 text-3xl sm:text-5xl mx-auto font-sans italic text-aqua underline "><p>Contact Me Here</p></li></a>
+                                         <a href="https://www.facebook.com/olly.mah.71" target="_blank" rel="noopener noreferrer" ><li className="text-center mb-8 text-3xl sm:text-5xl mx-auto font-sans italic text-aqua underline "><p>Contact Me Here</p></li></a>
                                       
                                         
                                     </ul>
